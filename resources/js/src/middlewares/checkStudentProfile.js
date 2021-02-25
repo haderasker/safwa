65671eb12ad95af15fb775f7ce6365d9985c8219
Ibrahim Sakr <ebrahimes@gmail.com>
2021-02-25 21:08:12 +0200

    fix register and login

--- resources/js/src/middlewares/checkStudentProfile.js
+++ resources/js/src/middlewares/checkStudentProfile.js
@@ -5,14 +5,14 @@ export const checkStudentProfile = async (to, from, next) => {
     const roles = store.getters['Authentication/getRoles']
 
     if (roles.indexOf('student') === -1) {
-        next()
+        return next()
     }
 
     const profile = store.getters['Authentication/getProfile']
 
     if (!profile.doctrine_id && to.name !== 'me') {
-        next('/me')
+        return next('/me')
     }
 
-    next()
+    return next()
 }
